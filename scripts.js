1b0ef5a9312baae0f67c847c84672084a6019afa
fabianairc <fabianaramirezzz@gmail.com>
2022-08-07 21:29:07 -0300

    sddv

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ const colorSquares = document.querySelectorAll(".color");
 colorSquares.forEach((square) => {
   square.addEventListener("click", selectColor);
 });
+
+const colorSquares = document.querySelectorAll(".color");
+
+colorSquares.forEach((square) => square.addEventListener("click", selectColor));
+
+const gridSquares = document.querySelectorAll(".painterBlock");
+
+gridSquares.forEach((square) => square.addEventListener("click", paintSquare));
+
+gridSquares.forEach((square) =>
+  square.addEventListener("mousemove", paintSquareOnMove)
+);
+
+const resetBtn = document.querySelector("#reset-btn");
+
+resetBtn.addEventListener("click", resetGrid);
